d0c18a3707fe84b9453d296a3912140bfcb139b0
kelvin.sousa <kelvin.rodrigues@tinnova.com.br>
2026-02-16 09:56:29 -0300

    remove log

--- pkg/freteRapidoApi/quote_v3.go
+++ pkg/freteRapidoApi/quote_v3.go
@@ -3,7 +3,6 @@ package freteRapidoApi
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 )
 
 func (c *Client) QuoteV3(
@@ -29,8 +28,6 @@ func (c *Client) QuoteV3(
 		"Content-Type": "application/json",
 		"Accept":       "application/json",
 	}
-
-	fmt.Printf("Request payload: %s\n", string(payload))
 
 	body, status, err := c.HTTP.Post(ctx, QUOTE_V3_PATH, headers, payload)
 	if err != nil {
